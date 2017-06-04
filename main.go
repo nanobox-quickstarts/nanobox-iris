90fe73895f36227c0b2fa6c3425d881cfc4dd118
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-06-05 02:08:52 +0300

    Update and make use of the existing templates folder

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
+	"github.com/kataras/iris/view"
 )
 
 func main() {
 	app := iris.New()
+	
+	// load the ./templates/**.html
+	templates := view.HTML("./templates", ".html"))
+	app.AttachView(templates)
+	
 	app.Handle("GET", "/", func(ctx context.Context) {
-		ctx.Writef("hello world\n")
+	        // bind the {{ .Name }}
+		ctx.ViewData("Name", "iris")
+		// render the ./templates/hi.html
+		ctx.View("hi.html")
 	})
 
 	app.Run(iris.Addr(":8080"))
